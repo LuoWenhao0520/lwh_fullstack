0f9413489c2c63599ed5c9c210f0fe20ef0909a5
LuoWenhao0520 <luowenhao0520@foxmail.com>
2021-03-06 16:02:52 +0800

    lwh

--- wxApp/douban/pages/home/home.js
+++ wxApp/douban/pages/home/home.js
@@ -7,14 +7,38 @@ Page({
   data: {
 
   },
+  // 获取城市信息
+  getCity(succeed){
+    // 先拿城市名称
+    // 拿到当前所在地的经纬度
+    wx.getLocation({
+      success: (res) => {
+        // console.log(res);
+        wx.request({
+          url: 'http://api.map.baidu.com/reverse_geocoding/v3',
+          data: {
+            ak: 'kP8qB4v0T1fNatjknr5NbL2q4Rv1VjLQ',
+            output :'json',
+            coordtype: 'wgs84',
+            location: `${res.latitude},${res.longitude}`
+          },
+          success: (res) => {
+            console.log(res);
+            // 拿豆瓣的api地址做接口请求，将获取到的城市名传给豆瓣api，拿到当前城市的数据
+          }
+        })
+      }
+    })
+  },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    this.getCity()
   },
 
+  
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
