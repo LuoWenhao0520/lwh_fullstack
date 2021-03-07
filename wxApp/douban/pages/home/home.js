2d595ef9f563ae150965dd5719f81943921870ce
LuoWenhao0520 <luowenhao0520@foxmail.com>
2021-03-07 22:05:40 +0800

    lwh

--- wxApp/douban/pages/home/home.js
+++ wxApp/douban/pages/home/home.js
@@ -5,7 +5,33 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    allMovies: [
+      {
+        title: "院线热映",
+        url: "/v2/movie/in_theaters",
+        movies: []
+      },
+      {
+        title: "新片榜",
+        url: "/v2/movie/new_movies",
+        movies: []
+      },
+      {
+        title: "口碑榜",
+        url: "/v2/movie/weekly",
+        movies: []
+      },
+      {
+        title: "北美票房榜",
+        url: "/v2/movie/us_box",
+        movies: []
+      },
+      {
+        title: "Top250",
+        url: "/v2/movie/top250",
+        movies: []
+      }
+    ]
   },
   // 获取城市信息
   getCity(succeed){
@@ -23,8 +49,14 @@ Page({
             location: `${res.latitude},${res.longitude}`
           },
           success: (res) => {
-            console.log(res);
+            // console.log(res);
             // 拿豆瓣的api地址做接口请求，将获取到的城市名传给豆瓣api，拿到当前城市的数据
+            let city = res.data.result.addressComponent.city
+            succeed && succeed(city)
+
+          },
+          fail:(err) => {
+            console.log(err);
           }
         })
       }
@@ -35,9 +67,49 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    this.getCity()
+    // this.getCity((city) => {
+    //   this.loadData(0,{city : city,apikey: '0df993c66c0c636e29ecbb5344252a4a'})
+    // })
+    this.loadData(1,{apikey: '0df993c66c0c636e29ecbb5344252a4a'})
+    this.loadData(2,{apikey: '0df993c66c0c636e29ecbb5344252a4a'})
+    this.loadData(3,{apikey: '0df993c66c0c636e29ecbb5344252a4a'})
+    // this.loadData(4,{apikey: '0df993c66c0c636e29ecbb5344252a4a'})
   },
 
+
+  loadData(idx,params) {
+    let obj = this.data.allMovies[idx]
+    let url = wx.db.url(obj.url)
+
+    wx.request({
+      url: url,
+      data: params,
+      header: {'content-type' : 'json'},
+      success: (res) => {
+        console.log(res);
+        let movies = res.data.subjects;
+
+        obj.movies = []
+        for(let i = 0;i < movies.length;i ++){
+          let element = movies[i]
+          let movie = element.subject || element
+          // 格式化星星
+          this.updateMovie(movie)
+
+          obj.movies.push(movie)
+
+        }
+        this.setData(this.data)
+      }
+    })
+  },
+
+  updateMovie(movie) {
+    if(!movie.rating.stars){
+      return
+    }
+    movie.numberStars = parseInt(movie.rating.stars)
+  },
   
   /**
    * 生命周期函数--监听页面初次渲染完成
